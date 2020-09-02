c5b1eddc158140c7801079f9dd43efcf9df85e34
Toldi2 Tamás <toldi2.tamas@ext.t-systems.hu>
2020-09-02 14:11:34 +0200

    Updates

---
+++ precache-manifest.1d43c00f6f80da5408698e046526aa51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200999e2364be9f237cfafd4197ff026",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a75b7192e8b1e942d28d",
+    "url": "/monsters-rolodex/static/css/main.e6cc6b39.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75b7192e8b1e942d28d",
+    "url": "/monsters-rolodex/static/js/main.95dc4ae4.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monster-rolodex/precache-manifest.33ed0bd290beafdc6cc8bc4de9876014.js"
+  "/monsters-rolodex/precache-manifest.1d43c00f6f80da5408698e046526aa51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monster-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
