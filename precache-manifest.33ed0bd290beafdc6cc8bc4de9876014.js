b87799544b58d428c7aa15f5be7e35b20a0f39db
Toldi2 Tamás <toldi2.tamas@ext.t-systems.hu>
2020-09-02 09:04:02 +0200

    Updates

---
+++ precache-manifest.33ed0bd290beafdc6cc8bc4de9876014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761dfd3dff90d1d3fe969d9a751bbd2e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "12f349345759d759b6dc",
+    "url": "/monster-rolodex/static/css/main.e6cc6b39.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f349345759d759b6dc",
+    "url": "/monster-rolodex/static/js/main.d90771e9.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
